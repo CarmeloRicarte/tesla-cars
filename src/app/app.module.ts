6d9952a4e9d4340e2b589729b2e346b6182e056b
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-03-02 20:43:44 +0100

    fix: delete unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { HomeModule } from './pages/home/home.module';
-import { ListCarsComponent } from './components/list-cars/list-cars.component';
 
 @NgModule({
   declarations: [AppComponent],
