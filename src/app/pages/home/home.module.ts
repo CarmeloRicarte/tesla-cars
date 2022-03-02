472105c2282b1abdc1e9d19c93e130566ff39236
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-03-02 22:08:28 +0100

    fix: delete unused import

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,7 +2,7 @@ import { ListCarsComponent } from '../../components/list-cars/list-cars.componen
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [HomeComponent, ListCarsComponent],
