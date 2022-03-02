d891d2d58cc5c1e97577df27129bed169b531c76
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-03-02 17:52:04 +0100

    test: migrate from jasmine/karma to jest and add jest-extented and jest-html-reporters

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  reporters: [
+    "default",
+    ["jest-html-reporters", {
+      "publicPath": "./html-tests-report",
+      "filename": "tests-report.html",
+      "openReport": true
+    }]
+  ]
+};
