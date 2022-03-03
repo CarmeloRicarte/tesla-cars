9709e863e79c8da069666c369b704d3cddc74766
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-03-03 12:09:11 +0100

    style: change order of imports

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,8 +1,8 @@
-import { ListCarsComponent } from '../../components/list-cars/list-cars.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home.component';
+import { ListCarsComponent } from '../../components/list-cars/list-cars.component';
 
 @NgModule({
   declarations: [HomeComponent, ListCarsComponent],
